ebf3bb5ecf8e9c7d9429643669622701f76e377d
Your Name <you@example.com>
2021-05-06 18:21:01 +0000

    Initial commit

---
+++ numberGuessing.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 Austin de Mora All rights reserved.
+//
+// Created by: Austin de Mora
+// Created on: May 2021
+// This program uses a try statement
+
+#include <iostream>
+#include <string>
+#include <random>
+
+main() {
+    // this function uses a try statement
+    std::string chosen_number_string;
+    int someRandomNumber;
+    int chosen_number;
+
+    std::cout << someRandomNumber << std::endl;
+
+    // input
+    std::cout << "Enter a number between 0 and 9: ";
+    std::cin >> chosen_number_string;
+    std::cout << "" << std::endl;
+
+    // process & output
+    try {
+        chosen_number = std::stoi(chosen_number_string);
+
+        std::random_device rseed;
+        std::mt19937 rgen(rseed());  // mersenne_twister
+        std::uniform_int_distribution<int> idist(0, 9);  // [0, 9]
+        someRandomNumber = idist(rgen);
+
+        if (chosen_number == someRandomNumber) {
+            // output
+            std::cout << "Correct!" << std::endl;
+        } else {
+            // output
+            std::cout << "The correct number is: "
+            << someRandomNumber << std::endl;
+        }
+    } catch (std::invalid_argument) {
+        // output
+        std::cout << "That was not an integer" << std::endl;
+    }
+}
